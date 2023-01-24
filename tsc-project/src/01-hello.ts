621b05e02448c137dd46b1961bf738a707b0e91f
Johan Fernandez <andresjfz10@gmail.com>
2023-01-24 17:29:45 -0500

    Initial TypeScript Configuration

---
+++ tsc-project/src/01-hello.ts
@@ -0,0 +1,2 @@
+const myName = 'Johan';
+console.log(myName);
---
+++ tsc-project/src/02-demo.ts
@@ -0,0 +1,81 @@
+/*
+  Activando los poderes de TypeScript en Javascript:
+  activar el analizador de código estático de TypeScript
+*/
+
+/*
+//@ts-check
+(()=> {
+  const myCart = [];
+  const products = [];
+  const limit = 2;
+
+  async function getProducts() {
+    const rta = await fetch('http://api.escuelajs.co/api/v1/products', {
+      mehtod: 'GET'
+    });
+    const data = await rta.parseJson();
+    products.concat(data);
+  }
+  function getTotal() {
+    const total = 0;
+    for (const i = 0; i < products.length(); i++) {
+      total += products[i].prize;
+    }
+    return total;
+  }
+  function addProduct(index) {
+    if (getTotal <= limit) {
+      myCart.push(products[index]);
+    }
+  }
+
+  await getProducts();
+  addProducto(1);
+  addProducto(2);
+  const total = getTotal();
+  console.log(total);
+  const person = {
+    name: 'Nicolas',
+    lastName: 'Molina'
+  }
+  const rta = person +  limit;
+  console.log(rta);
+});
+*/
+
+(async ()=> {
+  const myCart = [];
+  const products = [''];
+  const limit = 2;
+
+  async function getProducts() {
+    const rta = await fetch('http://api.escuelajs.co/api/v1/products', {
+      method: 'GET'
+    });
+    const data = await rta.json();
+    products.concat(data);
+  }
+  function getTotal() {
+    let total = 0;
+    for (let i = 0; i < products.length; i++) {
+      total += Number(products[i]);
+    }
+    return total;
+  }
+  function addProduct() {
+    getTotal()
+  }
+
+  await getProducts();
+  addProduct();
+  addProduct();
+  const total = getTotal();
+  console.log(total);
+  const person = {
+    name: 'Nicolas',
+    lastName: 'Molina'
+  }
+  // const rta = person +  limit;
+  // console.log(rta);
+});
